52f224a49d728338e8ff27ac3b74537574c9e17f
蔡昀辰 <tsaiyunchen@caiyunchendeMacBook-Air-4.local>
2025-09-12 17:57:43 +0800

    test

--- utils/iv_utils.py
+++ utils/iv_utils.py
@@ -260,5 +260,5 @@ def time_dependent_illumination_run(sock, wait_time=60):
 def time_dependent_dark_current():
     print('STEP: time dependent dark current()')
     click_RUN()
-    time.sleep(600)
+    time.sleep(480)
     click_STOP()
