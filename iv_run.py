af84e94f471218b8fb09e5608fc87e7d53cceca8
蔡昀辰 <tsaiyunchen@caiyunchendeMacBook-Air-4.local>
2025-09-12 15:23:09 +0800

    test

--- iv_run.py
+++ iv_run.py
@@ -60,15 +60,16 @@ for i in range(2):
     run_measurement()
     time.sleep(1)
     filename = filename_generator(material, device_number, measurement_type='idvg', condition=f'dark{i}-vg={vg_start}-vg={vg_end}-{i}')
-    # export_data(CSV_FOLDER, filename)
-
+    export_data(CSV_FOLDER, filename)
+    time.sleep(rest_time)
+    
 time.sleep(rest_time)
 
 # light idvg
 for i in range(2):
     illuminate_and_run(sock)
     filename = filename_generator(material, device_number, measurement_type='idvg', condition=f'light-{laser_wavelength}-{laser_power}-vg={vg_start}-vg={vg_end}-{i}')
-    # export_data(CSV_FOLDER, filename)
+    export_data(CSV_FOLDER, filename)
 
     time.sleep(rest_time)
 '''
