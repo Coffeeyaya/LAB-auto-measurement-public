9be8922c4862a641e4eb2b6db90ffe1125be2222
Coffeeyaya <coffee20120704@gmail.com>
2025-10-20 13:25:40 +0800

    test csv

--- server_csv.py
+++ server_csv.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import zmq
 
 CSV_FOLDER = Path(__file__).parent.parent / 'send_data'
+print('hi')
 os.makedirs(CSV_FOLDER, exist_ok=True)
 
 ZMQ_PORT = 5003  # separate port for CSV transfer
