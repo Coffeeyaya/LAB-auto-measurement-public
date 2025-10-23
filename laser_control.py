648fc605f258f3c86000e27607954d78a4bdad90
Coffeeyaya <coffee20120704@gmail.com>
2025-10-23 16:08:13 +0800

    test

--- laser_control.py
+++ laser_control.py
@@ -76,8 +76,7 @@ try:
         elif cmd == "FUNCTION" and laser_state != "FUNCTION":
             time_dependent(conn, grid, channel=6, power="15", num_peaks=1)  # single-channel FUNCTION
             # channels = np.arange(0, 8, 1, dtype=int)
-            # power_values = np.arange(10, 18, 1)
-            # power_values = power_values.astype(str)
+            # power_values = ["114", "85", "39", "39", "28", "21.5", "18.3", "18.2"]
             # time_dependent_wavelength(conn, grid, channels, power_values, on_time=10, off_time=30)
     conn.close()
 finally:
