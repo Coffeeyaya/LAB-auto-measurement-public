d859fc4c61e9df7b03e48141c84b1ad1bf08e557
Coffeeyaya <coffee20120704@gmail.com>
2025-10-23 13:49:16 +0800

    test

---
+++ laser_test.py
@@ -0,0 +1,34 @@
+import time
+import numpy as np
+from LabAuto.laser import init_AOTF, get_coord, change_power_function, move_and_click
+# from LabAuto.network import create_server, Connection
+
+
+laser_state = "OFF"
+
+def time_dependent_wavelength(grid, power_values):
+    global laser_state
+    laser_state = "FUNCTION"
+    
+
+    # here, assume all power percentages are pre-setted
+    channels = np.linspace(0, 7, 2, dtype=int)
+    
+    for channel, power in zip(channels, power_values):
+        on_coord = get_coord(grid, channel, "on")
+        change_power_function(grid, channel, power)
+        # turn on
+        move_and_click(on_coord)
+        time.sleep(0.9)
+
+        # turn off
+        move_and_click(on_coord)
+        time.sleep(0.9)
+
+    laser_state = "FUNCTION_DONE"
+
+grid = init_AOTF()
+if __name__ == "__main__":
+    power_values = np.arange(10, 18, 1)
+    power_values = power_values.astype(str)
+    time_dependent_wavelength(grid, power_values)
---
+++ test.py
@@ -0,0 +1,5 @@
+import numpy as np
+power_values = np.arange(10, 18, 1)
+power_values = power_values.astype(str)
+print(type(power_values[0]))
+print(len(power_values))
