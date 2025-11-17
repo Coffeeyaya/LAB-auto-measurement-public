32b602390828f83b327a8e0f8f465092842eb83f
Coffeeyaya <coffee20120704@gmail.com>
2025-11-17 16:54:27 +0800

    test

--- test/share.py
+++ test/share.py
@@ -1,15 +1,20 @@
 import http.server
 import socketserver
 from pathlib import Path
+import socket
 
 PORT = 8000
-DIRECTORY = Path(__file__).parent.parent / 'receive_data' / 'data'
-
+DIRECTORY = Path(__file__).parent.parent / 'send_data'
 class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+# Force IPv4
+class TCPServerV4(socketserver.TCPServer):
+    address_family = socket.AF_INET
+    allow_reuse_address = True
+
+with TCPServerV4(("", PORT), Handler) as httpd:
     print(f"Serving at http://0.0.0.0:{PORT}/")
     httpd.serve_forever()
 
