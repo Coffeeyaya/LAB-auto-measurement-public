0a21ad0b30f3183fc06ed4368b32bab788f73220
Coffeeyaya <coffee20120704@gmail.com>
2025-11-17 17:48:34 +0800

    test

--- test/reach.py
+++ test/reach.py
@@ -1,17 +1,25 @@
 import http.server
 import socketserver
 import socket
+import webbrowser
 
 PORT = 8000
 
-# Find local IPv4 addresses
+# Find local IPv4 addresses (LAN)
 hostname = socket.gethostname()
-ips = socket.gethostbyname_ex(hostname)[2]
-lan_ips = [ip for ip in ips if ip.startswith("192.") or ip.startswith("10.") or ip.startswith("172.")]
+all_ips = socket.gethostbyname_ex(hostname)[2]
+lan_ips = [ip for ip in all_ips if ip.startswith("192.") or ip.startswith("10.") or ip.startswith("172.")]
+
+if not lan_ips:
+    print("No LAN IPv4 address found. Using 0.0.0.0")
+    lan_ips = ["0.0.0.0"]
 
 print("Server will be reachable at:")
 for ip in lan_ips:
     print(f"  http://{ip}:{PORT}/")
+
+# Open first reachable IP in default browser automatically
+webbrowser.open(f"http://{lan_ips[0]}:{PORT}/")
 
 # Start HTTP server on all interfaces
 with socketserver.TCPServer(("", PORT), http.server.SimpleHTTPRequestHandler) as httpd:
