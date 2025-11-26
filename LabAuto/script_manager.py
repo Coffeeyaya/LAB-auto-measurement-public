8db8c5a9d3cfdf78000cfe4829261abad371f718
Coffeeyaya <coffee20120704@gmail.com>
2025-11-26 17:20:08 +0800

    try conda

--- LabAuto/script_manager.py
+++ LabAuto/script_manager.py
@@ -54,9 +54,10 @@ def run_script(script_name: str):
 
     elif system == "Windows":
         proc = subprocess.Popen(
-            [python_executable, full_path],
-            creationflags=subprocess.CREATE_NEW_CONSOLE
-        )
+        ["conda", "run", "-n", "myenv", "python", full_path],
+        creationflags=subprocess.CREATE_NEW_CONSOLE
+    )
+
 
     else:  # Linux
         proc = subprocess.Popen(
